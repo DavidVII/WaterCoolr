2b554a6a841f40e0f6777080b4c884c38a993fb0
David Becerra <David@redeyedesigner.com>
2013-07-08 21:49:38 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Watercoolr::Application.config.secret_key_base = 'bce74246bb7e7dd2a0d77d1027b954c5246f034fb181577630f03bf97ba1a29fb594a44d8bcc77a8b86d8a796ec6d7221e459b88df7b5e929c26266580990caa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Watercoolr::Application.config.session_store :cookie_store, key: '_watercoolr_session'
