5dfe190c61852cd0f96fd64861c54e69d5db8435
David Becerra <David@redeyedesigner.com>
2013-07-09 23:57:16 -0700

    add the link create action

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,23 @@
+class LinksController < ApplicationController
+  def show
+
+  end
+
+  def new
+    @link = Link.new    
+  end
+
+  def create
+    @link = Link.create(user_params)
+    if @link.save
+      flash[:success] = "Your link has been submitted!"
+    else
+      flash[:notice] = "Sorry, no goose..."
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:link).permit(:title, :url)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Watercoolr::Application.routes.draw do
   devise_for :users
   resources :pages
+  resources :links
   root to: 'pages#index'
 end
