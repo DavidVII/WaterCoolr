adeeba07f6df9bdf5a681d9a83ea3dcde00b502b
David Becerra <David@redeyedesigner.com>
2013-07-09 21:32:58 -0700

    add link model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130709060719) do
+ActiveRecord::Schema.define(version: 20130710043031) do
+
+  create_table "links", force: true do |t|
+    t.integer  "user_id"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
