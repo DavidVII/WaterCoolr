3dd6f3709945047350b33dc63b978b95e0065915
David Becerra <davidbecerra84@gmail.com>
2013-08-03 22:33:26 -0700

    add some copy to indicate votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,7 @@
 class VotesController < ApplicationController
   def create
-    # @vote = current_user.votes.create(vote_params)
     @vote = current_user.votes.where(link_id: vote_params[:link_id]).first || current_user.votes.create(vote_params)
     @vote.update_attributes(up: vote_params[:up])
-
-    flash[:notice] = @vote.errors.inspect
 
     redirect_to :back
   end
