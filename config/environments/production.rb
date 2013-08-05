6ca35d758547477a719956860a51b071896722f1
David Becerra <davidbecerra84@gmail.com>
2013-08-05 02:20:30 -0700

    update for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Watercoolr::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Devise
+  config.action_mailer.default_url_options = { :host => 'salty-stream-8727.herokuapp.com' }
 end
