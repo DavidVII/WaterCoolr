139c0e5e34cc53a03acfd6468d6127b692f98688
David Becerra <DavidBecerra84@gmail.com>
2013-08-11 04:08:12 -0700

    add ability to delete comments and clean the code up a bit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :correct_user, only: :destroy
 
   def create
     @comment = current_user.comments.create(comment_params)
@@ -13,10 +14,21 @@ class CommentsController < ApplicationController
    redirect_to :back 
   end
 
+  def destroy
+    @comment.destroy
+    flash[:notice] = "Your comment has been deleted"
+    redirect_to :back
+  end
+
   private
 
     def comment_params
       params.require(:comment).permit(:message, :link_id)
+    end
+
+    def correct_user
+      @comment = current_user.comments.find_by_id(params[:id])
+      redirect_to root_path if @comment.nil?
     end
 
 end
