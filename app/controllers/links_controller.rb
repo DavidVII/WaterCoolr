f595df9a05b3ab0bf8def4ca43db02e80711e34b
David Becerra <davidbecerra84@gmail.com>
2013-08-12 22:46:08 -0700

    add edit/delete methods to Links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,6 @@
 class LinksController < ApplicationController
   before_filter :authenticate_user!, except: :show
+  before_filter :correct_user, only: [:edit, :update, :destroy]
 
   def show
     @link = Link.find(params[:id])
@@ -22,9 +23,34 @@ class LinksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @link.update_attributes(link_params)
+      flash[:success] = "Your link has been updated"
+      redirect_to link_path(@link)
+    else
+      # FIXME: Temporary solution while working on links#edit
+      flash[:error] = "Error: #{@link.errors.full_messages}"
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    @link.destroy
+    flash[:notice] = "Your link has been deleted"
+    redirect_to root_path
+  end
+
   private
 
     def link_params
       params.require(:link).permit(:title, :url, :user_id)
+    end
+
+    def correct_user
+      @link = current_user.links.find_by_id(params[:id])
+      redirect_to root_path if @link.nil?
     end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,7 @@
 class Link < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
-  has_many :votes
+  has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   
   validates :title, presence: true
   validates :url, presence: true
