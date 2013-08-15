fda13039c25d33358f93f11ab8e9d4a3f426b572
David Becerra <DavidBecerra84@gmail.com>
2013-08-15 14:53:03 -0700

    add URL validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,5 +4,19 @@ class Link < ActiveRecord::Base
   has_many :votes, dependent: :destroy
   
   validates :title, presence: true
+
+  VALID_URL_REGEX = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+
+  before_validation :add_url_protocol
+
   validates :url, presence: true
+  validates :url, format: { with: VALID_URL_REGEX, :message => " is not valid" }
+
+  def add_url_protocol
+    unless self.url[URI::regexp(%w(http https))]
+      self.url = 'http://' + self.url
+    end
+  end
 end
+
+
