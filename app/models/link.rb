04f0446c18e30032857bfea44161f20600f199d4
David Becerra <davidbecerra84@gmail.com>
2013-08-15 21:37:35 -0700

    remove extra space

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ class Link < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
-  
+
   validates :title, presence: true
 
   VALID_URL_REGEX = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
